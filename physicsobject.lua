aef55311b9a6dcd8c16f597f23dc51ac86fe7ef9
Taylor Anderson <taylor-anderson@hotmail.ca>
2017-01-18 20:26:11 -0500

    updated physicsobject

--- physicsobject.lua
+++ physicsobject.lua
@@ -6,8 +6,20 @@ function PhysicsObject.new(x, y)
 	local self = setmetatable({}, PhysicsObject)
 	self.x = x
 	self.y = y
+	resolutions = {}
 	return self
 end
 
 function PhysicsObject:update(dt)
+	self.x = self.x + self.v.x
+	self.y = self.y + self.v.y
+end
+function PhysicsObject:addForce(force)
+	self.v.x = self.v.x + force.x
+	self.v.y = self.v.y + force.y
+	for i, v in pairs(resolutions) do
+		if (self:collide(i, self.x + self.v.x, self.y + self.v.y)) then
+			--TODO: add more detail to collide function so we can grab normal, pretty important
+		end
+	end
 end
